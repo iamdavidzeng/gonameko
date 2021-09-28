7a221933c8601f8c6b6fd618f1f626891eef56cd
zzy <15779712170@163.com>
2021-09-28 23:29:12 +0800

    Abstract connection layer

--- examples/main.go
+++ examples/main.go
@@ -4,12 +4,7 @@ import "github.com/iamdavidzeng/gonameko"
 
 func main() {
 	server := gonameko.Server{
-		Name:           "gonameko",
-		RabbitHostname: "localhost",
-		RabbitUser:     "guest",
-		RabbitPass:     "guest",
-		RabbitPort:     5672,
-		ContentType:    "application/json",
+		Name: "gonameko",
 	}
 
 	server.Run()
